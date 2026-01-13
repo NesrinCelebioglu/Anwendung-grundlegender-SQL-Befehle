446e3c7b49264960111ea3b139be7cb55ccd8088
Nesrin Celebioglu <nesrincelebioglu@gmail.com>
2026-01-13 23:35:52 +0100

    Update

--- SQLhausaufgabe1.sql
+++ SQLhausaufgabe1.sql
@@ -1,5 +1,5 @@
 ﻿
--- HR isimli bir veritabani olusturunuz ve bu veritabani içerisine gosterilen tablolari SQL kodu yazarak olusturunuz.
+-- Erstellen Sie eine Datenbank mit dem Namen HR und erstellen Sie in dieser Datenbank die dargestellten Tabellen, indem Sie SQL-Code schreiben.
 CREATE DATABASE HR
 
 CREATE TABLE PERSON
@@ -21,28 +21,28 @@ SALARY FLOAT)
 
 SELECT * FROM PERSON
 
---1. Tüm çalışanların ad, soyad ve maaş bilgilerini getiren SQL sorgusunu yazınız:
+--1. Schreiben Sie die SQL-Abfrage, die den Vornamen, Nachnamen und das Gehalt aller Mitarbeiter abruft.
 SELECT [NAME_],[SURNAME],[SALARY]
   FROM PERSON
 
---2. Sadece kadın çalışanların (GENDER = 'K') isim, soyisim ve doğum tarihlerini listeleyen bir sorgu yazınız.:
+--2. Schreiben Sie eine Abfrage, die nur die Namen, Nachnamen und Geburtsdaten der weiblichen Mitarbeiter (GENDER = 'K') auflistet.
 SELECT  [NAME_],[SURNAME],[BIRTHDATE]
  FROM PERSON
  WHERE GENDER = 'K'
  
---3. 2017 yılından sonra işe giren çalışanların isimlerini ve işe giriş tarihlerini listele:
+--3. Listen Sie die Namen und die Einstellungsdaten der Mitarbeiter auf, die nach dem Jahr 2017 eingestellt wurden.
 SELECT  [NAME_],[SURNAME],[INDATE]
   FROM PERSON
   WHERE INDATE > '2017-12-31' 
 
---4. Yeni bir çalışan ekle(Örnek: Ali Veli, TC No: 12345678901, Erkek, 1985-12-05 doğumlu, 2022-01-15 tarihinde işe girdi, Departman: 3, Pozisyon: 40, Maaş: 6000):
+--4. Fügen Sie einen neuen Mitarbeiter hinzu (Beispiel: Ali Veli, TC-Nummer: 12345678901, männlich, geboren am 05.12.1985, eingestellt am 15.01.2022, Abteilung: 3, Position: 40, Gehalt: 6000)
 INSERT INTO PERSON
 ([NAME_],[SURNAME],[TCNUMBER],[GENDER],[BIRTHDATE],[INDATE],[DEPARTMENTID],[POSITIONID],[SALARY])  
 VALUES ('Ali', 'Veli', '12345678901', 'E', '1985-12-05', '2022-01-15', 3, 40, 6000)
 
 SELECT * FROM PERSON
 
---5. Ferhat Cinar’ın maaşını 6000 olarak güncelle:
+--5. Aktualisieren Sie das Gehalt von Ferhat Cinar auf 6000.
 UPDATE PERSON
 SET  [SALARY]=6000  
 WHERE [NAME_]= 'Ferhat' AND [SURNAME]= 'CINAR'
@@ -50,55 +50,55 @@ WHERE [NAME_]= 'Ferhat' AND [SURNAME]= 'CINAR'
 SELECT * FROM PERSON
 WHERE [NAME_]= 'Ferhat' AND [SURNAME] = 'CINAR'
 
---6. Deniz Eravcı’yı veri tabanından sil:
+--6. Löschen Sie Deniz Eravcı aus der Datenbank.
 
 DELETE FROM PERSON WHERE [NAME_]= 'Deniz' AND [SURNAME] = 'ERAVCI'
 
 SELECT * FROM PERSON WHERE [NAME_]= 'Deniz' 
 
---7. Doğum yılı 1960’tan önce olan çalışanları listele:
+--7. Listen Sie die Mitarbeiter auf, die vor 1960 geboren wurden.
 SELECT [NAME_],[SURNAME],[BIRTHDATE]
 FROM   PERSON
 WHERE [BIRTHDATE] < '1960-01-01'
 
---8. Doğum tarihi 1960’tan önce ve maaşı 5000’den yüksek olan çalışanları listele:
+--8. Listen Sie die Mitarbeiter auf, deren Geburtsdatum vor 1960 liegt und deren Gehalt über 5000 liegt.
 SELECT [NAME_],[SURNAME],[BIRTHDATE],[SALARY]
 FROM   PERSON
 WHERE [BIRTHDATE] < '1960-01-01' AND [SALARY]> 5000
 
---9. Departman ID’si 4 olan veya maaşı 5500’den fazla olan çalışanları listele:
+--9. Listen Sie die Mitarbeiter auf, deren Abteilungs-ID 4 ist oder deren Gehalt über 5500 liegt.
 SELECT [NAME_],[SURNAME],[DEPARTMENTID],[SALARY]
 FROM   PERSON
 WHERE [DEPARTMENTID]=4  OR [SALARY]> 5500
 
---10. Çıkış tarihi (OUTDATE) NULL olan çalışanların maaşlarına %10 zam yap:
+--10. Erhöhen Sie das Gehalt der Mitarbeiter um 10 %, deren Austrittsdatum (OUTDATE) NULL ist
 UPDATE PERSON
 SET [SALARY] = [SALARY]* 1.10  
 WHERE [OUTDATE] IS NULL
 
 SELECT * FROM PERSON WHERE [OUTDATE] IS NULL
 
---11. 2015’ten önce işe giren ve maaşı 5000’den düşük olan çalışanları sil:
+--11. Löschen Sie die Mitarbeiter, die vor 2015 eingestellt wurden und deren Gehalt unter 5000 liegt.
 DELETE FROM PERSON
 WHERE [INDATE]< '2015-01-01'  
 AND [SALARY] < 5000
 
---12. Veri setinde kaç farklı departman olduğunu listele:
+--12. Listen Sie, wie viele verschiedene Abteilungen im Datensatz vorhanden sind.
 
 SELECT DISTINCT [DEPARTMENTID] FROM PERSON 
 
---Veri setinde kaç farklı departman olduğunu ise:
+--Ermitteln Sie, wie viele verschiedene Abteilungen im Datensatz vorhanden sind.
 
 SELECT COUNT(DISTINCT [DEPARTMENTID] ) AS  unique_department_count  
 FROM PERSON
 
---13. Çalışanları maaşlarına göre en yüksekten en düşüğe sırala:
+--13. Sortieren Sie die Mitarbeiter nach ihrem Gehalt von höchstem zu niedrigstem.
 SELECT [NAME_],[SURNAME],[SALARY]
 FROM PERSON
 ORDER BY [SALARY] DESC
 
 
---14. En yüksek maaş alan 5 çalışanı listele:
+--14. Listen Sie die 5 Mitarbeiter mit dem höchsten Gehalt auf.
 SELECT TOP 5 [NAME_],[SURNAME],[SALARY]
 FROM PERSON
 ORDER BY [SALARY] DESC
